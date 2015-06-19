fb0cb82aa693e1b85dc9c6d617f24d4dbf3b8146
Andrew Leech <anl@planetinnovation.com.au>
2015-06-19 17:24:16 +1000

    Make importing a bit easier

--- lib/__init__.py
+++ lib/__init__.py
@@ -20,3 +20,4 @@
 # 2. Altered source versions must be plainly marked as such, and must not be
 #    misrepresented as being the original software.
 # 3. This notice may not be removed or altered from any source distribution.
+import dbapi2 as sqlightning
